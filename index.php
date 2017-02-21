534f27a8778a8ae8606361a67e10da271ead5baa
William Keating <keating49@gmail.com>
2017-02-21 16:17:06 -0500

    php2

--- index.php
+++ index.php
@@ -1,22 +1,44 @@
-<?php
-if(isset($_POST['submit'])){
-    $to = "bam326@gmail.com"; // this is your Email address
-    $from = $_POST['email']; // this is the sender's Email address
-    $first_name = $_POST['first_name'];
-    $last_name = $_POST['last_name'];
-    $subject = "Form submission";
-    $subject2 = "Copy of your form submission";
-    $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
-    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
 
-    $headers = "From:" . $from;
-    $headers2 = "From:" . $to;
-    mail($to,$subject,$message,$headers);
-    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
-    // You can also use header('Location: thank_you.php'); to redirect to another page.
-    // You cannot use header and echo together. It's one or the other.
-    }
+<?php
+	if (isset($_POST["submit"])) {
+		$name = $_POST['name'];
+		$email = $_POST['email'];
+		$message = $_POST['message'];
+		$human = intval($_POST['human']);
+		$from = 'Demo Contact Form';
+		$to = 'bam326@gmail.com';
+		$subject = 'Test';
+
+		$body = "From: $name\n E-Mail: $email\n Message:\n $message";
+
+		// Check if name has been entered
+		if (!$_POST['name']) {
+			$errName = 'Please enter your name';
+		}
+
+		// Check if email has been entered and is valid
+		if (!$_POST['email'] || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+			$errEmail = 'Please enter a valid email address';
+		}
+
+		//Check if message has been entered
+		if (!$_POST['message']) {
+			$errMessage = 'Please enter your message';
+		}
+		//Check if simple anti-bot test is correct
+		if ($human !== 5) {
+			$errHuman = 'Your anti-spam is incorrect';
+		}
+
+// If there are no errors, send the email
+if (!$errName && !$errEmail && !$errMessage && !$errHuman) {
+	if (mail ($to, $subject, $body, $from)) {
+		$result='<div class="alert alert-success">Thank You! I will be in touch</div>';
+	} else {
+		$result='<div class="alert alert-danger">Sorry there was an error sending your message. Please try again later</div>';
+	}
+}
+	}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -139,28 +161,46 @@ if(isset($_POST['submit'])){
       <p>(703) 371-2415</p>
       <p>Or you can send me a message here</p>
 
-    <form id="form1" action="mailto:bam326@gmail.com" method="get">
-            <div style="margin-bottom:10px">
-                <label for="text">Name</label><br>
-                <input required type="text" id="text" name="text" value="">
-            </div>
-
-            <div style="margin-bottom:10px">
-                <input type="radio" id="radio1" name="radio" value="radio 1 value" checked>
-                <label for="radio1">Dog Walking</label><br>
-                <input type="radio" id="radio2" name="radio" value="radio 2 value">
-                <label for="radio2">Daycare</label>
-            </div>
-
-            <div style="margin-bottom:10px">
-                <label for="textarea">Message</label><br>
-                <textarea cols="60" rows="5" name="textarea" id="textarea"></textarea>
-            </div>
-
-            <div>
-                <button type="submit" name="Submit">Submit</button>
-            </div>
-        </form>
+      <form class="form-horizontal" role="form" method="post" action="index.php">
+      	<div class="form-group">
+      		<label for="name" class="col-sm-2 control-label">Name</label>
+      		<div class="col-sm-10">
+      			<input type="text" class="form-control" id="name" name="name" placeholder="First & Last Name" value="<?php echo htmlspecialchars($_POST['name']); ?>">
+      			<?php echo "<p class='text-danger'>$errName</p>";?>
+      		</div>
+      	</div>
+      	<div class="form-group">
+      		<label for="email" class="col-sm-2 control-label">Email</label>
+      		<div class="col-sm-10">
+      			<input type="email" class="form-control" id="email" name="email" placeholder="example@domain.com" value="<?php echo htmlspecialchars($_POST['email']); ?>">
+      			<?php echo "<p class='text-danger'>$errEmail</p>";?>
+      		</div>
+      	</div>
+      	<div class="form-group">
+      		<label for="message" class="col-sm-2 control-label">Message</label>
+      		<div class="col-sm-10">
+      			<textarea class="form-control" rows="4" name="message"><?php echo htmlspecialchars($_POST['message']);?></textarea>
+      			<?php echo "<p class='text-danger'>$errMessage</p>";?>
+      		</div>
+      	</div>
+      	<div class="form-group">
+      		<label for="human" class="col-sm-2 control-label">2 + 3 = ?</label>
+      		<div class="col-sm-10">
+      			<input type="text" class="form-control" id="human" name="human" placeholder="Your Answer">
+      			<?php echo "<p class='text-danger'>$errHuman</p>";?>
+      		</div>
+      	</div>
+      	<div class="form-group">
+      		<div class="col-sm-10 col-sm-offset-2">
+      			<input id="submit" name="submit" type="submit" value="Send" class="btn btn-primary">
+      		</div>
+      	</div>
+      	<div class="form-group">
+      		<div class="col-sm-10 col-sm-offset-2">
+      			<?php echo $result; ?>
+      		</div>
+      	</div>
+      </form> 
 
 
 
